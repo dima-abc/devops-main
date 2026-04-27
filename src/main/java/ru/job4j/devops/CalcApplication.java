6f67f6ac0371f916770d334d61585524956130c4
dima-abc <haoos@inbox.ru>
2026-04-27 15:21:29 +0300

    Жизненный цикл сборки Gradle [#505351]

--- src/main/java/ru/job4j/devops/CalcApplication.java
+++ src/main/java/ru/job4j/devops/CalcApplication.java
@@ -3,10 +3,13 @@ package ru.job4j.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Класс с main методом.
+ */
 @SpringBootApplication
 public class CalcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalcApplication.class, args);
+    }
 }
--- src/main/java/ru/job4j/devops/controllers/CalcController.java
+++ src/main/java/ru/job4j/devops/controllers/CalcController.java
@@ -5,15 +5,30 @@ import org.springframework.web.bind.annotation.*;
 import ru.job4j.devops.models.Result;
 import ru.job4j.devops.models.TwoArgs;
 
+/**
+ * Контроллер.
+ */
 @RestController
 @RequestMapping("calc")
 public class CalcController {
+    /**
+     * Сложение.
+     *
+     * @param twoArgs {@link TwoArgs}
+     * @return {@link ResponseEntity}
+     */
     @PostMapping("summarise")
     public ResponseEntity<Result> summarise(@RequestBody TwoArgs twoArgs) {
         var result = twoArgs.getFirst() + twoArgs.getSecond();
         return ResponseEntity.ok(new Result(result));
     }
 
+    /**
+     * Умножение.
+     *
+     * @param twoArgs {@link TwoArgs}
+     * @return {@link ResponseEntity}
+     */
     @PostMapping("times")
     public ResponseEntity<Result> times(@RequestBody TwoArgs twoArgs) {
         var result = twoArgs.getFirst() * twoArgs.getSecond();
